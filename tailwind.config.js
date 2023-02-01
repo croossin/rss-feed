e4886c36fa714608ba41a9d9cabee0c983c576a9
Chase Roossin <croossin94@gmail.com>
2023-02-01 06:05:43 -0800

    Get layout imported

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Satoshi", ...defaultTheme.fontFamily.sans],
+      },
+      spacing: {
+        18: "4.5rem",
+        112: "28rem",
+        120: "30rem",
+      },
+    },
   },
   plugins: [],
 };
